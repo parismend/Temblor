42ff7867161bce7b4c8a4c335de8d40e7883a305
Paris Mendez <paris.e.mendez@gmail.com>
2017-09-20 13:37:22 -0500

    Distancia entre coordenadas

---
+++ dist_coor.py
@@ -0,0 +1,19 @@
+import geopy as gp
+from gp.distance import vincenty
+
+def op_base(file):
+    base = []
+    with open (file, 'r') as sq:
+        base = sq.read()
+
+def cal_dist(lat,long):
+    cerc = []
+    for x in base:
+        dist = vincenty((base[x][12],base[x][13]), (lat,long)).meters
+        if dist < 100:
+            cerc.append(base[x])
+    return cerc
+
+if __name__ == '__main__':
+    op_base()
+    aaa = cal_dist()
