1c6bb705dc61f9185ec7c7779449d4f23646354c
eardil <eardi.nolmo@gmail.com>
2017-09-20 14:35:03 -0500

    distancia con checkeo

--- string_dist.py
+++ string_dist.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 from fuzzywuzzy import fuzz
@@ -6,7 +7,7 @@ from fuzzywuzzy import fuzz
 def distancia_string(lista):
     arreglo = np.zeros([len(lista), len(lista)])
     for i in range(0, len(lista)):
-        for j in range(i, len(lista)):
+        for j in range(i + 1, len(lista)):
             arreglo[i, j] = fuzz.token_set_ratio(
                 lista[i], lista[j]
             )
@@ -15,12 +16,31 @@ def distancia_string(lista):
 
 def indices_repetidos(lista, corte):
     arreglo = distancia_string(lista)
-    return np.where(arreglo >= corte)[1]
+    return np.where(arreglo >= corte), arreglo
 
 
 if __name__ == "__main__":
-    df = pd.DataFrame.from_csv('necesidades.csv')
-    distancia_calle = distancia_string(df.calle.tolist())
-    lista_calles = [df.calle.tolist()[i] + ' ' + df.entrecalles.tolist()[i]
+    df = pd.DataFrame.from_csv('datos/necesidades.csv')
+    distancia_calle = indices_repetidos(df.calle.tolist(), 90)
+    lista_calles = [str(df.calle.tolist()[i]) + ' ' +
+                    str(df.entrecalles.tolist()[i])
                     for i in range(df.shape[0])]
-    distancia_calles = distancia_string(df.calle.tolist())
+    distancia_calles, dist = indices_repetidos(lista_calles, 90)
+    repetidos = np.unique(distancia_calles[1])
+    print(len(repetidos) / len(lista_calles))
+
+    calles1 = []
+    calles2 = []
+    distancia = []
+    for i in range(distancia_calles[0].shape[0] - 1):
+        for j in range(distancia_calles[0].shape[0] - 1):
+            calles1.append(distancia_calles[0][i])
+            calles2.append(distancia_calles[1][j])
+            distancia.append(dist[distancia_calles[0][i],
+                                  distancia_calles[1][j]])
+
+    pd.DataFrame({
+        'calle1': [lista_calles[x] for x in calles1],
+        'calle2': [lista_calles[x] for x in calles2],
+        'distancia': distancia
+    }).to_csv('datos/distancias.csv')
