47a55c7caa331ba0c742c5cfbd936211baea6316
Paco Mekler <oscar.mekler@gmail.com>
2017-09-20 20:21:50 -0500

    veamos...

--- etls/todo.py
+++ etls/todo.py
@@ -1,5 +1,6 @@
 import luigi
-# from googlesheets.limpia_etl.googlesheets import get_Data_temblor
+import re
+from utils.danio_lib import dir_correct,obtain_latlong,get_credentials,get_Data_temblor,insert_Data_temblor,estructura_sheet
 from sengrid_handler import _send_error
 
 
@@ -21,6 +22,30 @@ class LimpiaGoogleSheets(luigi.Task):
         with self.output().open('w') as output_file:
             output_file.write('Hola google')
 
+        data = get_Data_temblor()
+        info = estructura_sheet(data)
+        info_pub = info.drop([
+            'Nombre del Informante (esta información no será pública)',
+            'Teléfono de Contacto (esta información no será pública)'],
+            axis=1)
+    
+        calles = info_pub['Calle'].tolist()
+        numeros = info_pub['Número Exterior  o Aproximado (escribe sólo el número)'].tolist()
+        lati = []
+        longi = []
+        for i in range(info_pub.shape[0]):
+            lat_aux, lon_aux = obtain_latlong(dir_correct(
+                calles[i], numeros[i]))
+            lati.append(lat_aux)
+            longi.append(lon_aux)
+        info_pub.columns = [re.sub('[<>{}\|]', '', x) for x in info_pub.columns]
+        info_pub.columns = [re.sub('\(.*\)', '', x) for x in info_pub.columns]
+        info_pub.columns = [x[0:60] for x in info_pub.columns]
+        info_pub['latitud'] = lati
+        info_pub['longitud'] = longi
+        info_pub.to_csv("tmp/corrida_{}.csv".format(self.query_date))
+
+
     def output(self):
         # print ("tmp/corrida_{}.csv".format(self.query_date))
         return luigi.LocalTarget("tmp/corrida_{}.csv".format(self.query_date))
@@ -38,5 +63,3 @@ class RunAll(luigi.Task):
 
     def output(self):
         return luigi.LocalTarget("tmp/_END")
-
-luigi.run()
