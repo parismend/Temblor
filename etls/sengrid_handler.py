7b5c3fba66ef9cdfc78e09947d4d40a0b676c014
Frank <akura11.tt@gmail.com>
2017-09-20 19:53:51 -0500

    Retirando llave

--- etls/sengrid_handler.py
+++ etls/sengrid_handler.py
@@ -6,7 +6,7 @@ import datetime
 def _send_error(task, exception):
     response = requests.post(
         "https://api.mailgun.net/v3/sandbox82ec51efe6ff46d8aef7968f9eb56af7.mailgun.org/messages",
-        auth=("api", os.environ.get('MAILGUN_API_KEY', 'key-83c82733faa51a845a1428ee5e67d2a3')),
+        auth=("api", os.environ.get('MAILGUN_API_KEY', '')),
         data={
             "from": "ETL TEMBLOR <postmaster@sandbox82ec51efe6ff46d8aef7968f9eb56af7.mailgun.org>",
             "to": os.environ.get('EMAIL_DESTINATION', "ETL BOX <{}>".format(os.environ.get('ETL_BOX_EMAIL', 'francisco@opianalytics.com'))),
