fedf852d42d7fbfa45782b9a6bac9485c276e44f
eardil <eardi.nolmo@gmail.com>
2017-09-21 12:54:08 -0500

    OCD

--- pullcdb2.py
+++ pullcdb2.py
@@ -15,12 +15,19 @@ def descarga_pandas(link):
 if __name__ == '__main__':
     ayuda_nec_link = 'https://descifra-admin.carto.com/api/v2/sql?q=select%20*%20from%20%22descifra-admin%22.ayudanecesitada'
     ayuda_vol_link = 'https://descifra-admin.carto.com/api/v2/sql?q=select%20*%20from%20%22descifra-admin%22.ayudavoluntaria'
-    droplist = ['cartodb_id', 'contacto', 'estatus', 'the_geom','the_geom_webmercator', 'latlong']
+    droplist = ['cartodb_id',
+                'contacto',
+                'estatus',
+                'the_geom',
+                'the_geom_webmercator',
+                'latlong']
 
     # REQUERIDA
     # Descargar df ayuda requerida y cambiar nombres
     df_nec = descarga_pandas(ayuda_nec_link)
-    df_nec = df_nec.rename(index=str, columns={"fechaalta": "Timestamp", "descripcion": "Víveres Faltantes"})
+    df_nec = df_nec.rename(index=str,
+                           columns={"fechaalta": "Timestamp",
+                                    "descripcion": "Víveres Faltantes"})
 
     # Filtrar columnas null y separar latlong en latitud y longitud
     df_nec = df_nec[df_nec.latlong.notnull()]
@@ -31,13 +38,18 @@ if __name__ == '__main__':
 
     # Tirar columnas no requeridas y convertir datetime
     df_nec = df_nec.drop(droplist, axis=1)
-    df_nec['Timestamp'] = df_nec['Timestamp'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ').strftime('%m/%d/%Y %H:%M:%S'))    
+    df_nec['Timestamp'] = df_nec['Timestamp'].apply(
+        lambda x: datetime.datetime.strptime(x,
+            '%Y-%m-%dT%H:%M:%SZ').strftime('%m/%d/%Y %H:%M:%S'))
 
 
     # OFRECIDA
     # Descargar df ayuda ofrecida y cambiar nombres
     df_vol = descarga_pandas(ayuda_vol_link)
-    df_vol = df_vol.rename(index=str, columns={"latlon": "latlong", "fechaalta": "Timestamp", "descripcion": "Víveres Sobrantes"})
+    df_vol = df_vol.rename(index=str, columns={
+        "latlon": "latlong",
+        "fechaalta": "Timestamp",
+        "descripcion": "Víveres Sobrantes"})
 
     # Filtrar columnas null y separar latlong en latitud y longitud
     df_vol = df_vol[df_vol.latlong.notnull()]
@@ -48,12 +60,13 @@ if __name__ == '__main__':
 
     # Tirar columnas no requeridas y convertir datetime
     df_vol = df_vol.drop(droplist, axis=1)
-    df_vol['Timestamp'] = df_vol['Timestamp'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ').strftime('%m/%d/%Y %H:%M:%S'))    
-
+    df_vol['Timestamp'] = df_vol['Timestamp'].apply(
+        lambda x: datetime.datetime.strptime(
+            x, '%Y-%m-%dT%H:%M:%SZ').strftime('%m/%d/%Y %H:%M:%S'))
 
     # FORMS
     # Abrir el CSV
-    df_csv = pd.read_csv('datos.csv', header=0, skiprows=[1,1])
+    df_csv = pd.read_csv('datos.csv', header=0, skiprows=[1, 1])
     df_csv = df_csv.drop(['Unnamed: 0', 'Unnamed: 1'], axis=1)
 
     # Concatenar
@@ -66,11 +79,15 @@ if __name__ == '__main__':
 
     # Info adicional
     for col in danios.columns[danios.columns.str.contains('alta')]:
-        danios.loc[danios[col] == '', col] = 'Si tienes info entra a: http://bit.ly/Verificado19s'
-        danios.loc[danios[col].isnull(), col] = 'Si tienes info entra a: http://bit.ly/Verificado19s'
+        danios.loc[danios[col] == '',
+                   col] = 'Si tienes info entra a: http://bit.ly/Verificado19s'
+        danios.loc[danios[col].isnull(),
+                   col] = 'Si tienes info entra a: http://bit.ly/Verificado19s'
     for col in danios.columns[danios.columns.str.contains('obra')]:
-        danios.loc[danios[col] == '', col] = 'Si tienes info entra a: http://bit.ly/Verificado19s'
-        danios.loc[danios[col].isnull(), col] = 'Si tienes info entra a: http://bit.ly/Verificado19s'
+        danios.loc[danios[col] == '',
+                   col] = 'Si tienes info entra a: http://bit.ly/Verificado19s'
+        danios.loc[danios[col].isnull(),
+                   col] = 'Si tienes info entra a: http://bit.ly/Verificado19s'
 
     # Guardar a csv
     filename = 'danios.csv'
