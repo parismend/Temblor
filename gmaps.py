ab86edcdc1053a2598f827524c838523867203c6
Miguel Angel Riego Alrich <miguel.a.riego@gmail.com>
2017-09-22 11:02:11 -0500

    Update gmaps.py
    Vectoricé operación. Ya casi está

--- gmaps.py
+++ gmaps.py
@@ -1,36 +1,24 @@
 # -*- coding: utf-8 -*-
-import googlemaps
+import googlemaps 
 import pandas as pd
 from datetime import datetime
 
-gmaps = googlemaps.Client(key='')
+gmaps = googlemaps.Client(key='AIzaSyB8w0ss2pvnSjh2LJhLzku4FX71KQnO48Q ')
 
-# Geocoding an address
 def p_read(csv):
-    df = pd.read_csv(csv, encoding="utf-8")
-    df.reset_index()
-    #df = df.drop('Column1', axis=1)
+    df = pd.read_csv(csv)
     return df
 
-def geocodel(x):
-    try: 
-        geocode_result = gmaps.geocode_result(df['Calle'][x]+' '+df['Número'][x]+', '+df['Colonia'][x]+', '+df['Delegación'][x])
-        dic = geocode_result[0]['geometry']['location']
-    except:
-        dic = {'lat': 'no encontrado', 'lng': 'no encontrado'}    
-    return dic
-
+def g_geocode(calle, numero, colonia, delegacion):
+    geocode_result = gmaps.geocode((str(calle)+" "+str(numero)+", "+str(colonia)+", "+str(delegacion)))
+    geocode_locdict = geocode_result[0]['geometry']['location']
+    return geocode_locdict
 
 if __name__ == '__main__':
-    df = p_read('test.csv')
-    df = df[df.Calle.notnull()]
-    # cómo iterar bien aquí?
-    if ['Calle'][x] != 0 and df['Colonia'][x] != 0:
-        df['latitud'] = df['latitud'].apply(lambda x: geocodel(x)['lat'])
-        df['longitud'] = df['longitud'].apply(lambda x: geocodel(x)['lng'])
-        print(df['longitd'][x])
+    df = p_read('test.csv')    
+    is_valid = df['Calle'].notnull() | df['Colonia'].notnull()
+    is_missing = df['latitud'].isnull() | df['longitud'].isnull()
+    apply_vector = is_valid * is_missing
 
-"""
-df['Calle'] = [x[1] for x in Calle]
-df['Timestamp'] = df['Timestamp'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ').strftime('%m/%d/%Y %H:%M:%S'))
-"""    
+    # Falta vsacar los varios argumentos o volver en un string
+    df[apply_vector]['latitud'] = df.loc[apply_vector,["Calle", "Número", "Colonia", "Delegación o municipio"]].apply(lambda x: print(x))
