6fb732d4e5dbeb059085acea31381f95a8d29939
Juan Zinser <juanzinser@gmail.com>
2017-09-23 19:53:59 -0500

    adds acopio ayuda optima etl

--- etls/googlesheets/acopio_ayuda_optima/etl_unificando.sh
+++ etls/googlesheets/acopio_ayuda_optima/etl_unificando.sh
@@ -19,8 +19,8 @@ python3 etl_unificando.py
 echo 'Se generó acopio_ayuda_optima.csv'
 
 echo "Vamos a subir todo a google"
-gsutil -m cp harvard_mit.csv gs://sismocdmx/acopio-ayuda-optima/
-gsutil -m cp harvard_mit.csv gs://sismocdmx/acopio-ayuda-optima/acopio_ayuda_optima$fecha.csv
+gsutil -m cp acopio_ayuda_optima.csv gs://sismocdmx/acopio-ayuda-optima/
+gsutil -m cp acopio_ayuda_optima.csv gs://sismocdmx/acopio-ayuda-optima/acopio_ayuda_optima$fecha.csv
 gsutil acl ch -u AllUsers:R gs://sismocdmx/acopio-ayuda-optima/acopio_ayuda_optima.csv
 bq load --replace --autodetect --source_format CSV --skip_leading_rows 1 sismocdmx.harvard-mit gs://sismocdmx/acopio-ayuda-optima/acopio_ayuda_optima.csv
 
