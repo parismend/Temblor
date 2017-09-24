7df869a337d6d20c613e7ac3933004d5040e8523
Juan Zinser <juanzinser@gmail.com>
2017-09-24 14:06:05 -0500

    adds 4/10

--- etls/googlesheets/acopio_ayuda_optima/etl_unificando.py
+++ etls/googlesheets/acopio_ayuda_optima/etl_unificando.py
@@ -109,18 +109,18 @@ if __name__ == '__main__':
 
     data = get_Data_temblor()
     unificando = estructura_sheet(data)
-
-    Descripcion = unificando['DESCRIPCIÓN ']
-    Del_Ent = unificando['DELEGACIÓN/ENTIDAD FEDERATIVA ']
+    print(unificando.colums)
+    Descripcion = unificando['DESCRIPCIÓN']
+    Del_Ent = unificando['DELEGACIÓN/ENTIDAD FEDERATIVA']
     Colonia = unificando['COLONIA ']
     Referencia = unificando['Lugar de REFERENCIA']
     Direccion = unificando['DIRECCIÓN']
     Hora_Atencion = unificando['HORARIOS DE ATENCIÓN']
     Transporte = unificando['REQ TRANSPORTE PARA MOVER VÍVERES A OTRO LUGAR']
-    Viveres = unificando['Mandan víveres hacia ']
-    Salidas = unificando['Urgencia de salida ']
+    Viveres = unificando['Mandan víveres hacia']
+    Salidas = unificando['Urgencia de salida']
     Necesitan = unificando['NECESITAN ']
-    Ex_Viveres = unificando['OFRECEN VÍVERES A QUIEN LO NECESITEN ']
+    Ex_Viveres = unificando['OFRECEN VÍVERES A QUIEN LO NECESITEN']
     Fechas = unificando['FECHAS 19s ']
     s19 = unificando['s19 POR FAVOR NO MODIFICAR ']
     latitud = unificando['LAT ']
